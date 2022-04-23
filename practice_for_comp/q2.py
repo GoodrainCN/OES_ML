24d0be299b3eb96c0b4949dcf61314ed94133139
Goodrain Sun <77870996+GoodrainCN@users.noreply.github.com>
2022-04-23 09:57:36 -0700

    update

--- practice_for_comp/q2.py
+++ practice_for_comp/q2.py
@@ -1,4 +1,7 @@
 def circle_check(x,y,length):
     x_center = x + length/2
     y_center = y + length/2
-    
+    check_list = []
+    for i in range(x):
+        for n in range(x):
+            print(i,n)
